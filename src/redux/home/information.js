8824f9319a0d873b8b02e31c54dabe8f8915e553
Roland-Ntwali <rolandntwali@gmail.com>
2023-05-22 21:40:26 +0200

    Install Redux and create Redux store

---
+++ src/redux/home/information.js
@@ -0,0 +1,40 @@
+import { createAsyncThunk, createSlice} from "@reduxjs/toolkit";
+
+const url = 'https://disease.sh/v3/covid-19/countries'
+
+export const getCountries = createAsyncThunk('countries/fetchCountries', async () => {
+    const resolve = await fetch(url)
+    const countries = await resolve.json();
+    console.log(countries)
+    return countries; 
+});
+
+const DataSlice = createSlice({
+    name: 'countries',
+    initialState: {
+        countries: [],
+        status: null,
+    },
+
+    reducers: {
+
+    },
+
+    extraReducers(builder) {
+        builder.addCase(getCountries.pending, (state) => {
+            const isPending = state;
+            isPending.status = 'loading';
+        });
+        builder.addCase(getCountries.fulfilled, (state, action) => {
+            const fulfilled = state;
+            fulfilled.status = 'fulfilled'
+            fulfilled.countries = action.payload
+        })
+
+
+
+
+    },
+
+});
+export default DataSlice.reducer;
