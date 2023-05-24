134723e6572f0634ce51cff34eb98469f20a919e
Roland-Ntwali <rolandntwali@gmail.com>
2023-05-24 18:29:09 +0200

    Made changes in mockFetch

--- src/mock/mockFectch.js
+++ src/mock/mockFectch.js
@@ -1,29 +1,28 @@
-/* eslint-disable */ 
 const country = {
-    country: '',
-    countryInfo: [],
-  
-  };
-  
-  export default async function mockFetch(url) {
-    switch ('https://disease.sh/v3/covid-19/countries') {
-      case 'https://disease.sh/v3/covid-19/countries': {
-        return {
-          ok: true,
-          status: 200,
-          json: async () => countries,
-        };
-      }
-      case 'https://disease.sh/v3/covid-19/countries':
-      {
-        return {
-          ok: true,
-          status: 200,
-          json: async () => country,
-        };
-      }
-      default: {
-        throw new Error(`Unhandled request: ${'https://disease.sh/v3/covid-19/countries'}`);
-      }
+  country: '',
+  countryInfo: [],
+};
+
+const countries = []; // Define the 'countries' variable
+
+export default async function mockFetch(url) {
+  switch (url) { // Use the 'url' parameter in the switch statement
+    case 'https://disease.sh/v3/covid-19/countries': {
+      return {
+        ok: true,
+        status: 200,
+        json: async () => countries, // Use the 'countries' variable
+      };
+    }
+    case 'https://disease.sh/v3/covid-19/country': { // Change the duplicate case label
+      return {
+        ok: true,
+        status: 200,
+        json: async () => country,
+      };
+    }
+    default: {
+      throw new Error(`Unhandled request: ${url}`); // Use the 'url' parameter
     }
   }
+}
